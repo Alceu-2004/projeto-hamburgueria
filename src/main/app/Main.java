d5455a0b823fce33bd0a557743c2f916e567649c
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-23 10:02:50 -0300

    feat(app): integração dos padrões no fluxo principal

---
+++ src/main/app/Main.java
@@ -0,0 +1,25 @@
+package app;
+
+import singleton.*;
+import factorymethod.*;
+import decorator.*;
+import abstractfactory.*;
+import bridge.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(ConfiguracaoSistema.getInstancia().getStatus());
+
+        Hamburguer h = HamburguerFactory.criar("simples");
+        h = new Bacon(h);
+        System.out.println(h.preparar());
+
+        ComboFactory combo = new ComboTradicionalFactory();
+        System.out.println(combo.criarBebida().servir());
+
+        Pagamento pagamento = new PedidoPagamento(new Pix());
+        pagamento.pagar(30);
+    }
+}
