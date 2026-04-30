64f2988fad9b6cb90021dabc16e08cf4e151f810
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-04-30 13:30:40 -0300

    feat: adicionei a interface Subject para gerenciamento de observadores

---
+++ src/main/observer/Subject.java
@@ -0,0 +1,7 @@
+package observer;
+
+public interface Subject {
+    void adicionarObserver(Observer observer);
+    void removerObserver(Observer observer);
+    void notificarObservers(String mensagem);
+}
