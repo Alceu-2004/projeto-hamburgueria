c3c260e12a9a0c4e6d4a4438b75b6e6568de7fe2
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-05-08 17:07:04 -0300

    feat: adiciona produto individual no padrão Composite

---
+++ src/main/composite/Produto.java
@@ -0,0 +1,22 @@
+package composite;
+
+public class Produto implements ItemPedido {
+
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    @Override
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String getNome() {
+        return nome;
+    }
+}
