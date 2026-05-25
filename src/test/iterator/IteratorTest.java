0a61e1c922055abb3794bc32888833982e204f4c
Alceu Vasconcellos Botelho dos Reis <thetecworld17@gmail.com>
2026-05-25 14:47:07 -0300

    test: adiciona testes do padrão Iterator

---
+++ src/test/iterator/IteratorTest.java
@@ -0,0 +1,47 @@
+package iterator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class IteratorTest {
+
+    @Test
+    void devePercorrerTodosPedidos() {
+
+        ListaPedidos pedidos = new ListaPedidos();
+
+        pedidos.adicionarPedido("X-Burger");
+        pedidos.adicionarPedido("X-Bacon");
+        pedidos.adicionarPedido("X-Salada");
+
+        IteradorPedido iterator = pedidos.criarIterador();
+
+        assertEquals("X-Burger", iterator.proximo());
+        assertEquals("X-Bacon", iterator.proximo());
+        assertEquals("X-Salada", iterator.proximo());
+        assertFalse(iterator.temProximo());
+    }
+
+    @Test
+    void deveIndicarQueHaProximoElemento() {
+
+        ListaPedidos pedidos = new ListaPedidos();
+
+        pedidos.adicionarPedido("Combo Tradicional");
+
+        IteradorPedido iterator = pedidos.criarIterador();
+
+        assertTrue(iterator.temProximo());
+    }
+
+    @Test
+    void deveIndicarColecaoVazia() {
+
+        ListaPedidos pedidos = new ListaPedidos();
+
+        IteradorPedido iterator = pedidos.criarIterador();
+
+        assertFalse(iterator.temProximo());
+    }
+}
